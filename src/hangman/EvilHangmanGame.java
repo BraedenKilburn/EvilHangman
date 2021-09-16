2ef13751cfc379fe9e4c8cdf6f3f1b627c5b8bc0
Braeden Kilburn <braedenk99@gmail.com>
2021-09-16 11:41:37 -0600

    Passes HangmanTest

--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -1,15 +1,17 @@
 package hangman;
 
-import com.sun.source.tree.Tree;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame
 {
-    private TreeSet<String> wordBank;
-    private TreeSet<Character> guessedLetters;
+    // Word Bank that holds all possible words that the program can pick from
+    // Updates after every guess, getting smaller each time the player makes a guess
+    private SortedSet<String> wordBank;
+    // Set of letters that has already been guessed by the player
+    private final SortedSet<Character> guessedLetters;
+    // Amount of guesses remaining before the player loses
     private int guessesLeft;
 
     /**
@@ -24,6 +26,7 @@ public class EvilHangmanGame implements IEvilHangmanGame
 
     /**
      * Constructor for EvilHangmanGame() with parameters
+     *
      * @param guesses number of guesses available to player before they lose
      */
     public EvilHangmanGame(int guesses)
@@ -57,7 +60,7 @@ public class EvilHangmanGame implements IEvilHangmanGame
             throw new EmptyDictionaryException();
 
         // File Input
-        try(Scanner scanner = new Scanner(dictionary))
+        try (Scanner scanner = new Scanner(dictionary))
         {
             // While there's input to scan
             while (scanner.hasNext())
@@ -70,7 +73,7 @@ public class EvilHangmanGame implements IEvilHangmanGame
             throw new EmptyDictionaryException();
 
         // Create a new TreeSet to hold all the words with the right wordLength
-        TreeSet<String> correctWordLengthBank = new TreeSet<>();
+        SortedSet<String> correctWordLengthBank = new TreeSet<>();
 
         // For every word in our entire wordBank
         for (String word : wordBank)
@@ -90,11 +93,12 @@ public class EvilHangmanGame implements IEvilHangmanGame
     }
 
     /**
-     * This function updates our wordBank TreeSet to only hold the words that have not been eliminated yet
+     * This function updates our wordBank to only hold the words that have not been eliminated yet
+     * These are words which the program can pick from and still be correct
      *
      * @param remainingWordsBank Set containing the words with the correct wordLength
      */
-    private void updateWordBank(TreeSet<String> remainingWordsBank)
+    private void updateWordBank(SortedSet<String> remainingWordsBank)
     {
         this.wordBank = remainingWordsBank;
     }
@@ -112,7 +116,171 @@ public class EvilHangmanGame implements IEvilHangmanGame
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException
     {
-        return null;
+        // Standardize all guesses to lowercase
+        guess = Character.toLowerCase(guess);
+
+        // Character has been guessed already
+        for (Character character : guessedLetters)
+        {
+            if (character == guess)
+                throw new GuessAlreadyMadeException();
+        }
+
+        // Character has not been guessed yet, add it to the Set
+        guessedLetters.add(guess);
+        guessesLeft--;
+
+        // Partition wordBank relative to the guessed letter
+        Map<String, SortedSet<String>> partition = new TreeMap<>();
+        partitionWordBank(partition, guess);
+
+        // Choose the largest subset in the partition to be the new wordBank
+        String newSubsetKey = getLargestSubsetKey(partition, guess);
+
+        // Update our wordBank to just hold this new subset of words
+        updateWordBank(partition.get(newSubsetKey));
+
+        // Return the set of all possible words
+        return this.wordBank;
+    }
+
+    /**
+     * From our current wordBank, we will partition each word found
+     * to build a partition of subsets of words that have the guessed
+     * letter in the same position(s).
+     *
+     * @param partition a map that keeps track of the words in each subset in the partition
+     * @param guess     character guessed by player
+     */
+    private void partitionWordBank(Map<String, SortedSet<String>> partition, char guess)
+    {
+        // For every word in our wordBank
+        for (String word : wordBank)
+        {
+            // What is the letter pattern for our guess
+            String pattern = getPatternString(guess, word);
+            // If our partition already contains a set using this pattern
+            if (partition.containsKey(pattern))
+            {
+                // Add this word to the correct pattern set
+                partition.get(pattern).add(word);
+            }
+            // If we haven't seen this pattern before
+            else
+            {
+                // Create a new set for the words with this pattern
+                SortedSet<String> newPatternSet = new TreeSet<>();
+                // Add our word option to this new set
+                newPatternSet.add(word);
+                // Add our newly found pattern to the partition
+                partition.put(pattern, newPatternSet);
+            }
+        }
+    }
+
+    /**
+     * This method will find the largest subset of potential words so that the
+     * program has the maximum number of options when it picks the word at the
+     * end of the game. If multiple subsets have the largest size, use the
+     * tiebreakers in the project specification.
+     *
+     * @param partition Map containing all the possible subsets of words
+     * @param guess     Player's guessed character
+     * @return The key (pattern) of the largest subset in our map
+     */
+    private String getLargestSubsetKey(Map<String, SortedSet<String>> partition, char guess)
+    {
+        // Size of the largest subset (size = the amount of words)
+        int maxSubsetSize = 0;
+        // Key of the largest subset
+        String largestSubsetKey = "";
+
+        // For every key (pattern) in our map
+        for (String pattern : partition.keySet())
+        {
+            // Do we need to update our size and key?
+            boolean replaceSubsetKey = false;
+
+            // If this pattern contains more strings than the current max
+            if (partition.get(pattern).size() > maxSubsetSize)
+            {
+                // This pattern is our new largest subset key
+                replaceSubsetKey = true;
+            }
+            // If this pattern contains the same amount of strings as the current max subset
+            else if (partition.get(pattern).size() == maxSubsetSize)
+            {
+                // If this pattern has more missing characters
+                if (count(pattern, '-') > count(largestSubsetKey, '-'))
+                    // Make pattern our new key
+                    replaceSubsetKey = true;
+                    // If this pattern has less guessed characters
+                else if (count(pattern, guess) < count(largestSubsetKey, guess))
+                    // Make pattern our new key
+                    replaceSubsetKey = true;
+                    // If they have the same amount of correct characters and blanks
+                else if (count(pattern, guess) == count(largestSubsetKey, guess))
+                {
+                    // If pattern's first occurrence of guess is further right than the current max
+                    if (largestSubsetKey.indexOf(guess) < pattern.indexOf(guess))
+                        // Make pattern our new key
+                        replaceSubsetKey = true;
+                }
+            }
+            // If we need to update our size and subset key
+            if (replaceSubsetKey)
+            {
+                maxSubsetSize = partition.get(pattern).size();
+                largestSubsetKey = pattern;
+            }
+        }
+        return largestSubsetKey;
+    }
+
+    /**
+     * Function that calculates the subset key for a word
+     * with the respect to a guessed letter
+     *
+     * @param guess the guessed character
+     * @param word  the word we want to build our pattern from
+     * @return pattern string for the word based on its characters
+     */
+    private String getPatternString(char guess, String word)
+    {
+        // Using a StringBuilder to build our pattern
+        StringBuilder patternBuilder = new StringBuilder();
+        // For every character in our word
+        for (int i = 0; i < word.length(); i++)
+        {
+            // If the character guessed is in our word
+            if (guess == word.charAt(i))
+                // Add it to our pattern in the correct position
+                patternBuilder.insert(i, guess);
+            else
+                // If not found, add a '-' instead of the letter as a placeholder
+                patternBuilder.insert(i, '-');
+        }
+
+        return patternBuilder.toString();
+    }
+
+    /**
+     * Function to count the number of occurrences of
+     * a character in a string
+     *
+     * @param word      word to inspect
+     * @param character character to compare
+     * @return number of occurrences of specified character in your word
+     */
+    private int count(String word, char character)
+    {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (word.charAt(i) == character)
+                count++;
+        }
+        return count;
     }
 
     /**
